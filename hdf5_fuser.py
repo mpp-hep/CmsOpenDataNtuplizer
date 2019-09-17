5fe49b4f69cd2bfa5eb16cd59303260024668b6d
Oliver Knapp <oliver.knapp@outlook.com>
2019-09-17 19:06:40 +0200

    improvement for fuser

--- hdf5_fuser.py
+++ hdf5_fuser.py
@@ -1,37 +1,9 @@
 import os
 import argparse
-import glob
-
 from multiprocessing import Pool
 
 import numpy as np
 import h5py
-import pickle
-
-
-def fuse(path, file_list):
-    data_fused = None
-
-    for f in file_list:
-
-        file_path = os.path.join(path, f)
-
-        if not (os.path.isfile(file_path) and f.endswith('.hdf5')):
-            continue
-
-        print('loading: ' + f)
-
-        # load hdf5 file
-        hdf5_file = h5py.File(file_path, "r")
-        data = hdf5_file['data'].value
-        hdf5_file.close()
-
-        if data_fused is None:
-            data_fused = data
-        else:
-            data_fused = np.append(data_fused, data, axis=0)
-
-    return data_fused
 
 
 def load(file_path):
@@ -62,31 +34,13 @@ if __name__ == '__main__':
 
     res = p.map(load, file_path_list)
 
+    print('loading finished. number of files loaded: ' + str(len(res)))
+    print('concatenating files')
     data_fused = np.concatenate(res, axis=0)
 
-    # for f in os.listdir(args.input):
-    #
-    #     file_path = os.path.join(args.input, f)
-    #
-    #     if not (os.path.isfile(file_path) and f.endswith('.hdf5')):
-    #         continue
-    #
-    #     print('loading: ' + f)
-    #
-    #     # load hdf5 file
-    #     hdf5_file = h5py.File(file_path, "r")
-    #     data = hdf5_file['data'].value
-    #     hdf5_file.close()
-    #
-    #     if data_fused is None:
-    #         data_fused = data
-    #     else:
-    #         data_fused = np.append(data_fused, data, axis=0)
-
-    print('loading finished. shape: ' + str(data_fused.shape))
+    print('concatenation finished. shape: ' + str(data_fused.shape))
 
     print('saving to: ' + args.output)
-    # np.save(args.output, data_fused)
 
     hdf5_file = h5py.File(args.output, "w")
     hdf5_file.create_dataset('data', data=data_fused, compression='gzip')
