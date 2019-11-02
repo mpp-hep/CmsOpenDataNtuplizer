134e5cd13fe258c28352344b26826f7d1092d292
Oliver Knapp <oliver.knapp@outlook.com>
2019-11-02 10:23:11 +0100

    improvement for compile_xsec_mix

--- compile_xsec_mix.py
+++ compile_xsec_mix.py
@@ -3,29 +3,27 @@ import h5py
 import sklearn
 
 if __name__ == '__main__':
-    data_files = ['/eos/home-o/oknapp/record_7719/data.hdf5',
-                  '/eos/home-o/oknapp/record_7721/data.hdf5',
-                  '/eos/home-o/oknapp/record_7722/data.hdf5',
-                  '/eos/home-o/oknapp/record_7723/data.hdf5',
-                  '/eos/home-o/oknapp/record_9863/data.hdf5',
-                  '/eos/home-o/oknapp/record_9864/data.hdf5',
-                  '/eos/home-o/oknapp/record_9865/data.hdf5']
+    base_dir = '/eos/home-o/oknapp/opendata_v2/'
+    output_file = '/eos/home-o/oknapp/opendata_v2/wjets_dyjets_mix.hdf5'
+
+    data_files = [base_dir + '7719/data.hdf5',  # DY1
+                  base_dir + '7721/data.hdf5',  # DY2
+                  base_dir + '7722/data.hdf5',  # DY3
+                  base_dir + '7723/data.hdf5',  # DY4
+                  base_dir + '9863/data.hdf5',  # W1
+                  base_dir + '9864/data.hdf5',  # W2
+                  base_dir + '9865/data.hdf5']  # W3
 
     xsec = np.array([561, 181, 51.1, 15, 4480, 1435, 304.2])
-    output_file = '/eos/home-o/oknapp/wjets_dyjets_mix.hdf5'
+    fraction = xsec / np.sum(xsec)
 
-    # data_files = ['/home/oliverkn/pro/6021/data_hlf.hdf5', '/home/oliverkn/pro/9865/data_hlf.hdf5']
-    # xsec = np.array([0.001, 0.002])
-    # output_file = '/home/oliverkn/pro/testseetst.hdf5'
+    print(fraction)
 
     max_length = -1
-
-    fraction = xsec / np.sum(xsec)
 
     # load data
     data_list = []
     for data_file in data_files:
-        print('loading ' + data_file)
         if data_file.endswith('.npy'):
             x = np.load(data_file)
         elif data_file.endswith('.hdf5'):
@@ -34,16 +32,23 @@ if __name__ == '__main__':
         data_list.append(x)
 
     # computing N s.t. fraction is possible
-    N = np.amin([data.shape[0] / f for data, f in zip(data_list, fraction)])
+    N = [data.shape[0] / f for data, f in zip(data_list, fraction)]
+    i_min = np.argmin(N)
+    print('limiting (%d): %s' % (N[i_min], data_files[i_min]))
+    N = N[i_min]
     N = int(N)
     if max_length > 0 and N > max_length:
         N = max_length
     print('N = ' + str(N))
 
+    # taking random subsets and meeting fractions
     for i in range(len(data_list)):
         N_i = int(fraction[i] * N)
+
         print('loading %s of %s' % (N_i, data_files[i]))
-        data_list[i] = data_list[i][0:N_i]
+        idx = np.arange(0, data_list[i].shape[0])
+        idx = np.random.choice(idx, N_i, replace=False)
+        data_list[i] = data_list[i][()][idx]
 
     for i in range(len(data_list)):
         print('%s: %s (%s)' % (data_files[i], data_list[i].shape, data_list[i].shape[0] / float(N)))
